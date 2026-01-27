48a4ebd0a8f0b60f9ef1b53b4f888c72ab286e4c
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-01-26 20:17:39 -0800

    Test enable x86 without libstdc++ change
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- qualcomm-software/embedded/scripts/build.sh
+++ qualcomm-software/embedded/scripts/build.sh
@@ -127,7 +127,7 @@ log "Configuring LLVM"
 mkdir -p "${BUILD_DIR}/llvm"
 pushd "${BUILD_DIR}/llvm" >/dev/null
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-  -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;RISCV" \
+  -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;RISCV;X86" \
   -DLLVM_EXTERNAL_PROJECTS="eld" \
   -DLLVM_EXTERNAL_ELD_SOURCE_DIR="llvm/tools/eld" \
   -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-unknown-linux-gnueabi" \
@@ -156,7 +156,7 @@ if [[ "${AARCH64_BUILD}" == "true" ]]; then
     -S "${SRC_DIR}/llvm" \
     -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR_AARCH64}" \
     -DLLVM_ENABLE_PROJECTS="llvm;clang;polly;lld;mlir" \
-    -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;RISCV" \
+    -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;RISCV;X86" \
     -DLLVM_HOST_TRIPLE="aarch64-linux-gnu" \
     -DLLVM_EXTERNAL_PROJECTS="eld" \
     -DLLVM_EXTERNAL_ELD_SOURCE_DIR="${SRC_DIR}/llvm/tools/eld" \
