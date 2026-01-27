4ff4312b1f7f603849f2a5a33979e9a249551ea4
Jonathon Penix <jpenix@quicinc.com>
2026-01-26 19:34:16 -0500

    [cpullvm] Refactor using framework from ATfE (#83)
    This is a big refactor of our initial cpullvm to use/follow how ATfE [1]
    is organized/built.
    
    Our motivation for heading in this direction is based on a few things:
    - Generally, we don't want to make direct changes to the various repos
      we use (we'll use a copy from upstream and apply temporary and limited
      patches on top where needed).
    - We need to support a reasonable amount of variants across multiple
      third-party libcs, with multilib support. We also want it to be
      relatively easy for outside individuals to be able to add their own
      variants for experimentation, etc.
    - We have to support a diverse set of host platforms for our toolchain.
    - We'd like to work towards enabling additional baremetal testing
      (compiler-rt, libc++) in a way that scales with our variants as per
      above. Additionally, we'd like to expand our ability to do
      toolchain-level testing/validation.
    
    The ATfE approach seems to offer a reasonable framework for each of these.
    Additionally, there's some prior art in this direction with ex: Zephyr's
    LLVM SDK. So, let's use this as a launching point to hopefully reduce
    the amount of groundwork we need to (re)do.
    
    Note that the builds this produces isn't 1:1 with our current cpullvm
    builds--MUSL variants, eld support, Linux configs, etc. will be added
    in subsequent PRs.
    
    Basically all the code added here is directly pulled from ATfE with some
    editorializing and removing of features, etc. that we do not need or need
    additional work and so are omitted for now. For reference, I was working
    off a version of ATfE at [2] with [3] additionally cherry-picked.
    
    Notable omissions from ATfE include:
    - newlib/llvm-libc. llvm-libc might be interesting for us eventually, but
      we aren't using it for now. We do not want to support newlib.
    - Shared header optimizations. We might want to re-introduce this, but
      for now it requires a bit of fixup for RISC-V so is omitted.
    - Toolchain-testing/samples. We should reintroduce something like this, but
      this requires additional work. Revisit later.
    
    [1] https://github.com/arm/arm-toolchain/tree/arm-software/arm-software/embedded
    [2] https://github.com/arm/arm-toolchain/commit/05b72a96b30b66d7b0fb402cf9885b9a286d49b6
    [3] https://github.com/arm/arm-toolchain/commit/cb069ebb28562f567e27f4f15d1bb00311e97d8a
    
    Signed-off-by: Jonathon Penix <jpenix@quicinc.com>

---
+++ qualcomm-software/embedded/cmake/generate_version_txt.cmake
@@ -0,0 +1,35 @@
+# The following line will look different depending on how you got this
+# source file. If you got it from a Git repository then it will contain
+# a string in the git pretty format with dollar symbols. If you got it
+# from a source archive then the `git archive` command should have
+# replaced the format string with the Git revision at the time the
+# archive was created. This is configured in the .gitattributes file.
+# In the former case, this script will run a Git command to find out the
+# current revision. In the latter case the revision will be used as is.
+set(cpullvm_COMMIT "$Format:%H$")
+
+if(NOT ${cpullvm_COMMIT} MATCHES "^[a-f0-9]+$")
+    execute_process(
+        COMMAND git -C ${CPULLVMToolchain_SOURCE_DIR} rev-parse HEAD
+        OUTPUT_VARIABLE cpullvm_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
+
+# Supported libcs are all in a separate repo
+set(base_library ${LLVM_TOOLCHAIN_C_LIBRARY})
+
+execute_process(
+    COMMAND git -C ${${base_library}_SOURCE_DIR} rev-parse HEAD
+    OUTPUT_VARIABLE ${base_library}_COMMIT
+    OUTPUT_STRIP_TRAILING_WHITESPACE 
+    COMMAND_ERROR_IS_FATAL ANY
+)
+set(LLVM_TOOLCHAIN_C_LIBRARY_URL ${${base_library}_URL})
+set(LLVM_TOOLCHAIN_C_LIBRARY_COMMIT ${${base_library}_COMMIT})
+
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/VERSION.txt.in
+    ${CMAKE_CURRENT_BINARY_DIR}/VERSION.txt
+)
