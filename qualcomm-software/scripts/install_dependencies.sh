c413eb8c50a84b2a66be0a3e934ec60f33e2fcf5
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-02-07 12:25:03 -0800

    wip nightlies added, refactoring out deps first
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

---
+++ qualcomm-software/scripts/install_dependencies.sh
@@ -0,0 +1,14 @@
+# Copyright (c) 2025, Arm Limited and affiliates.
+# Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Changes from Qualcomm Technologies, Inc. are provided under the following license:
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. 
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# This script installs extra build dependencies for building and testing the
+# toolchain.
+
+# Install meson. eld support was added in v1.9.0, so we need at least that.
+pip install meson==1.10.0
