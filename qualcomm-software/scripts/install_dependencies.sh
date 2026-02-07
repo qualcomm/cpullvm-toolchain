f692dff191abf7a50e01401e9a05521ee21d93bc
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-02-07 12:25:03 -0800

    [cpullvm] Add "Native Runtime" nightly workflow, AArch64 Linux only
    This adds a new "Native Runtime" workflow that is intended to be an
    alternative to our currently nightly, which builds (and hopefully soon
    packages) runtimes only on x86_64 Linux. This way, we have at least some
    coverage of both ways of building the runtimes.
    
    To start, this only covers AArch64 Linux, but will be extended to Windows
    in a later PR.
    
    There's also quite a bit of duplication between this nightly and our
    primary one, but as we package the x86_64 Linux runtimes into other targets
    in the primary nightly, the two should start diverging again. Also, this
    adds yet another temporary build script--these are expected to go away
    soon, so I didn't bother with trying to share code between them.
    
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

---
+++ qualcomm-software/scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Install meson. eld support was added in v1.9.0, so we need at least that.
+pip install meson==1.10.0
