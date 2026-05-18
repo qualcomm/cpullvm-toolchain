aaf213fc6d38dba9f28d17587e48dbc6223b46a9
nshanmug <nshanmug@qti.qualcomm.com>
2026-05-18 08:41:03 -0700

    [cpullvm] use apt-get -y and add libc++ dependencies
    These packages are required for ELD unit tests
    
    Signed-off-by: nshanmug <nshanmug@qti.qualcomm.com>

--- qualcomm-software/scripts/install_dependencies.sh
+++ qualcomm-software/scripts/install_dependencies.sh
@@ -3,7 +3,10 @@
 
 sudo apt-get update
 # Used by lldb
-sudo apt-get install swig libedit-dev
+sudo apt-get install -y swig libedit-dev
+
+# Install clang, libc++ and libc++abi
+sudo apt-get install -y clang libc++-dev libc++abi-dev libclang-rt-20-dev
 
 # Install meson. eld support was added in v1.9.0, so we need at least that.
 pip install meson==1.10.0
