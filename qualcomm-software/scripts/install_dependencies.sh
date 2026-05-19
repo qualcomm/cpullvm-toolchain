118299fe266b70d85a8fbd8724cce040ef118fec
nshanmug <nshanmug@qti.qualcomm.com>
2026-05-18 18:44:47 -0700

    Remove libc++abi-dev from list of installs
    As its not needed for eld tests
    
    Signed-off-by: nshanmug <nshanmug@qti.qualcomm.com>

--- qualcomm-software/scripts/install_dependencies.sh
+++ qualcomm-software/scripts/install_dependencies.sh
@@ -2,11 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 sudo apt-get update
-# Used by lldb
-sudo apt-get install -y swig libedit-dev
-
-# Install libc++ and libc++abi required for eld tests
-sudo apt-get install -y libc++-dev libc++abi-dev
+# Install swig and libedit-dev required for lldb and
+# libc++-dev required for eld tests
+sudo apt-get install -y swig libedit-dev libc++-dev
 
 # Install meson. eld support was added in v1.9.0, so we need at least that.
 pip install meson==1.10.0
